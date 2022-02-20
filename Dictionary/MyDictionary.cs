980cce9110fd91f1941a5248698615d47d60bf9e
Bora Kaya <bora.587@hotmail.com>
2022-02-20 18:38:24 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    internal class MyDictionary<Tkey,Tvalue>
+       
+    {
+        Tkey[] keys;
+        Tvalue[] values;
+        public MyDictionary()
+        {
+            keys = new Tkey[0]; 
+            values = new Tvalue[0];
+        }
+
+        public void Add(Tkey key, Tvalue value)
+        {
+            Tkey[] tempKeys = keys;
+            Tvalue[] tempValues = values;
+            keys = new Tkey[keys.Length + 1];
+            values = new Tvalue[keys.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                tempKeys[i] = keys[i];
+            }
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                tempValues[i] = values[i];
+            }
+
+            key = keys[keys.Length - 1];
+            value = values[values.Length - 1];
+        }
+
+            
+
+        public int Count
+        {
+            get { return keys.Length; }
+            
+        }
+
+    }
+
+    }
+
+
---
+++ Dictionary/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+            myDictionary.Add(1, "Bora");
+            myDictionary.Add(2, "Istanbul");
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
